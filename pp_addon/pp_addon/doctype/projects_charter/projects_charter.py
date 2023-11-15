edba258c2cead47439a98d07ad74e892d6f733e6
yasser1248 <yasserelbana@gmail.com>
2023-11-15 18:02:32 +0300

    Project Charter Milestone-Risk-Quality and Print Format

--- pp_addon/pp_addon/doctype/projects_charter/projects_charter.py
+++ pp_addon/pp_addon/doctype/projects_charter/projects_charter.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2023, magdyabouelatta and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Projectscharter(Document):
-	pass
-
-
-import frappe
-frappe.scrub
+    def save(self, *args, **kwargs):
+        self.set("projects_milestone", frappe.get_value("Projects Milestone", {"project": self.get("project")}, "name"))
+        self.set("projects_risks", frappe.get_value("Projects Risks", {"project": self.get("project")}, "name"))
+        self.set("project_quality", frappe.get_value("Project quality", {"project": self.get("project")}, "name"))
+        return super().save(*args, **kwargs)
