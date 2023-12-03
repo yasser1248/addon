7a7bb46a78575a57fa0479ed2d7bd2308998cb7d
yasser1248 <yasserelbana@gmail.com>
2023-12-03 18:15:19 +0300

    Add the draggable functionality to Dialog in task_tree.js

--- pp_addon/pp_addon/doctype/task/task_tree.js
+++ pp_addon/pp_addon/doctype/task/task_tree.js
@@ -56,7 +56,7 @@ function click_edit_node(node, btn) {
                 let d = new frappe.ui.Dialog({
                     title: r.message.name,
                     fields: get_fields(r.message),
-                    size: 'large', // small, large, extra-large 
+                    size: 'small', // small, large, extra-large 
                     primary_action_label: 'Save',
                     async primary_action(values) {
                         if (values.description) delete values.description;
@@ -81,6 +81,9 @@ function click_edit_node(node, btn) {
                     }
                 });
                 d.show();
+                setTimeout(() => {
+                    dragElement(document.getElementsByClassName("modal")[0]);
+                }, 2 * 1000);
             }
         },
         error: (r) => {console.log(r);},
@@ -96,4 +99,48 @@ function get_fields(doc) {
         }
     });
     return fields
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementsByClassName("modal-header")[0]) {
+        // if present, the header is where you move the DIV from:
+        document.getElementsByClassName("modal-header")[0].onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
