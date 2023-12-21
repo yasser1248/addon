1d014b4254b4ecc79c3f22fb8bebf7e3ff92ae60
yasser1248 <yasserelbana@gmail.com>
2023-12-21 14:29:14 +0300

    from_date to_date rqd:1

--- pp_addon/pp_addon/report/employee_daily_work/employee_daily_work.js
+++ pp_addon/pp_addon/report/employee_daily_work/employee_daily_work.js
@@ -7,13 +7,15 @@ frappe.query_reports["Employee Daily Work"] = {
 			"fieldname": "from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
-			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1,
+			// "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
 		},
 		{
 			"fieldname": "to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
-			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			// "default": frappe.datetime.get_today(),
 		},
 		{
 			"fieldname": "project",
