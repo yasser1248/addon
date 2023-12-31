95f3ef4ec187f8501e904f560e4f5837609a2df3
yasser1248 <yasserelbana@gmail.com>
2023-12-31 12:16:43 +0300

    Comment pp_addon/endpoints/api.py

--- pp_addon/endpoints/api.py
+++ pp_addon/endpoints/api.py
@@ -1,15 +1,15 @@
-"""
-This module contains the API endpoints for the application.
-"""
-import json
+# """
+# This module contains the API endpoints for the application.
+# """
+# import json
 
-import frappe
+# import frappe
 
 
-@frappe.whitelist()
-def get_employee_attendance(*args, **kwargs):
-    print(frappe.form_dict)
-    report = frappe.get_doc("Report", "Employees Attendance Details")
-    filters = {"from_date": "01-12-2023", "to_date": "31-12-2023"}
-    _, data =report.execute_module({})
-    return data
+# @frappe.whitelist()
+# def get_employee_attendance(*args, **kwargs):
+#     print(frappe.form_dict)
+#     report = frappe.get_doc("Report", "Employees Attendance Details")
+#     filters = {"from_date": "01-12-2023", "to_date": "31-12-2023"}
+#     _, data =report.execute_module({})
+#     return data
