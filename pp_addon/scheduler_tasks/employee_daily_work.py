9401f9955cb616ddbe998aca8627b9713fb8746c
yasser1248 <yasserelbana@gmail.com>
2024-01-04 16:39:01 +0300

    Change code in pp_addon/scheduler_tasks/employee_daily_work.py

--- pp_addon/scheduler_tasks/employee_daily_work.py
+++ pp_addon/scheduler_tasks/employee_daily_work.py
@@ -9,4 +9,5 @@ from frappe.email.doctype.auto_email_report.auto_email_report import send_now
 
 def send_report():
     doc = frappe.get_doc("Auto Email Report", "Employee Daily Work All")
-    send_now(doc.name)
+    doc.send()
+    # send_now(doc.name)
