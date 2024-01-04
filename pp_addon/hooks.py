7ec8864a45a0ded6c33da6387b9b5f406d9d71f1
yasser1248 <yasserelbana@gmail.com>
2024-01-04 17:16:21 +0300

    Try to solve the scheduler function checkout

--- pp_addon/hooks.py
+++ pp_addon/hooks.py
@@ -149,7 +149,7 @@ scheduler_events = {
         "01 00 * * *": ["pp_addon.tasks.cron"],
         "0 14 * * 4": ["pp_addon.scheduler_tasks.employee_daily_work.send_report"],
         "0 10 * * 0,1,2,3,4": ["pp_addon.scheduler_tasks.employee_checkin_checkout.checkin"],
-        "0 18 * * 0,1,2,3,4": ["pp_addon.scheduler_tasks.employee_checkin_checkout.checkout"],
+        "0 18 * * 0,1,2,3,4": ["pp_addon.scheduler_tasks.employee_checkout.check_out"],
         "0 18 * * 0,1,2,3,4": ["pp_addon.scheduler_tasks.employee_checkin_checkout.employees_not_done_any_tasks"],
     },
     # 	"all": [
---
+++ pp_addon/scheduler_tasks/employee_checkout.py
@@ -0,0 +1,5 @@
+from pp_addon.scheduler_tasks.employee_checkin_checkout import checkout
+
+
+def check_out():
+    checkout()
