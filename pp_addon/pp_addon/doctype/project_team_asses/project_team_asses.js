ae9020b7db5acfbc80d4bca46a23204776a2b3aa
yasser1248 <yasserelbana@gmail.com>
2024-01-16 12:25:51 +0300

    Updata Project team asses DocType

--- pp_addon/pp_addon/doctype/project_team_asses/project_team_asses.js
+++ pp_addon/pp_addon/doctype/project_team_asses/project_team_asses.js
@@ -2,7 +2,63 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Project team asses', {
-	// refresh: function(frm) {
+	project: function(frm) {
+		if (frm.doc.project) {
+			add_child_table_row(frm);
+		}
+	},
 
-	// }
+	employee: function(frm) {
+		if (frm.doc.project) {
+			add_child_table_row(frm);
+		}
+	}
 });
+
+
+function add_child_table_row(frm) {
+	values_promise = frappe.call({
+		method: "pp_addon.pp_addon.doctype.project_team_asses.project_team_asses.get_start_end_date",
+		args: {"project": frm.doc.project},
+	});
+	values_promise.then(
+		(r) => {
+			if (Object.keys(r.message).length > 0) {
+				const monthList = generateMonthList(r.message.start_date, r.message.end_date);
+				console.log(monthList);
+				for (let i=0; i<monthList.length; i++) {
+					frm.add_child("items", {"from": monthList[i].firstDay, "to":  monthList[i].lastDay});
+				}
+				frm.refresh_field("items");
+			}
+		}
+	);
+}
+
+
+function generateMonthList(startDateStr, endDateStr) {
+	// Parse start and end dates
+	const startDate = new Date(startDateStr);
+	const endDate = new Date(endDateStr);
+
+	// Initialize the result array
+	const monthList = [];
+
+	// Iterate through months
+	let currentDate = new Date(startDate);
+	while (currentDate <= endDate) {
+		const firstDayOfMonth = currentDate;
+		const lastDayOfMonth = frappe.datetime.add_months(currentDate, 1);
+
+		// Push the first and last day of the month to the result array
+		monthList.push({
+			firstDay: firstDayOfMonth.toISOString().split('T')[0],
+			lastDay: lastDayOfMonth
+		});
+
+		// Move to the next month
+		currentDate = new Date(lastDayOfMonth);
+	}
+
+	return monthList;
+}
--- pp_addon/pp_addon/doctype/project_team_asses/project_team_asses.py
+++ pp_addon/pp_addon/doctype/project_team_asses/project_team_asses.py
@@ -1,8 +1,17 @@
 # Copyright (c) 2023, magdyabouelatta and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Projectteamasses(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_start_end_date(*args, **kwargs):
+    data = frappe.get_all("Project", ["start_date", "end_date",], {"name": kwargs.get("project")})
+    if len(data) == 1:
+        return data[0]
+    else:
+        return None
