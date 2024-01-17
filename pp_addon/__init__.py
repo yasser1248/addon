0cffbef03d426f089381c8f2ae7870f941573385
yasser1248 <yasserelbana@gmail.com>
2024-01-17 12:34:02 +0300

    Remove monket patch

--- pp_addon/__init__.py
+++ pp_addon/__init__.py
@@ -1,11 +1,11 @@
 
 __version__ = '0.0.1'
 
-import frappe
-from frappe import _
-import frappe.model.delete_doc
+# import frappe
+# from frappe import _
+# import frappe.model.delete_doc
 
-import pp_addon.model.delete_doc
+# import pp_addon.model.delete_doc
 
 # Monkey patch to allow Cascading deletion
 
