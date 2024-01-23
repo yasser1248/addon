68baf9af813a9d6af5e73fec48c81095c0424289
yasser1248 <yasserelbana@gmail.com>
2024-01-23 12:25:33 +0300

    Update Project Expenses, add from_to , from, to

--- pp_addon/pp_addon/doctype/project_expenses/project_expenses.py
+++ pp_addon/pp_addon/doctype/project_expenses/project_expenses.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2024, magdyabouelatta and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ProjectExpenses(Document):
-	pass
+	def validate(self):
+		if self.get("expenses") and self.get("from") and self.get("to"):
+			for row in self.get("expenses"):
+				if self.get("from") <= row.get("date") <= self.get("to"):
+					pass
+				else:
+					frappe.throw(f"Date should be between {self.get('from')} and {self.get('to')}")
+
+
+@frappe.whitelist()
+def get_aleardy_month_expenses(*args, **kwargs):
+    data = frappe.get_all("Project Expenses", ["from_to", "from", "to"], {"project": kwargs.get("project"),})
+    if len(data) > 0:
+        return {"from_to": [f"{item.get('from')} : {item.get('to')}" for item in data]}
+    else:
+        return None
