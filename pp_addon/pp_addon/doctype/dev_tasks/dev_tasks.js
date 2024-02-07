cd81e219da9a7282bf89b57ae29e70a670a85b9e
yasser1248 <yasserelbana@gmail.com>
2024-02-07 12:08:26 +0300

    add doctype for dev tasks

---
+++ pp_addon/pp_addon/doctype/dev_tasks/dev_tasks.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, magdyabouelatta and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Dev Tasks", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ pp_addon/pp_addon/doctype/dev_tasks/dev_tasks.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, magdyabouelatta and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class DevTasks(Document):
+	pass
